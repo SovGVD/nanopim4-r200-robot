16519010a732f1b9a42aef6ba9aa5bc7ae346438
Gleb Devytakin <sovgvd@gmail.com>
2019-04-06 23:54:14 +0000

    gstreamer-rockchip patch for h264 basline

---
+++ patches/gstreamer-rockchip/rebuild.sh
@@ -0,0 +1,3 @@
+make uninstall && make clean && sync && ./autogen.sh --disable-rkximage && sync && make -j1 && sync && make install && sync
+cp /usr/local/lib/gstreamer-1.0/libgstrockchipmpp.so /usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstrockchipmpp.so
+cp /usr/local/lib/gstreamer-1.0/libgstrockchipmpp.la /usr/lib/aarch64-linux-gnu/gstreamer-1.0/libgstrockchipmpp.la
